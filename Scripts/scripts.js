0ce54114e92ca8c15c7a46305b3923e24e870e3d
di0nar4p <glaucoplay@live.com>
2023-04-19 10:23:41 -0300

    Adicionada função parcial do keyboard

---
+++ Scripts/scripts.js
@@ -0,0 +1,49 @@
+function keyboard(id){
+
+
+    switch (id) {
+        case 1:
+            document.getElementById('visor').innerHTML = 1;
+            break;
+
+            case 2:
+                document.getElementById('visor').innerHTML = 2;
+                break;
+                
+                case 3:
+                    document.getElementById('visor').innerHTML = 3;
+                    break;
+                    
+                    case 4:
+                        document.getElementById('visor').innerHTML = 4;
+                        break;
+
+                        case 5:
+                            document.getElementById('visor').innerHTML = 5;
+                            break;
+
+                            case 6:
+                                document.getElementById('visor').innerHTML = 6;
+                                break;
+
+                                case 7:
+                                    document.getElementById('visor').innerHTML = 7;
+                                    break;
+
+                                    case 8:
+                                        document.getElementById('visor').innerHTML = 8;
+                                        break;
+                                    
+                                        case 9:
+                                            document.getElementById('visor').innerHTML = 9;
+                                            break;
+                                            
+                                            case '÷':
+                                                document.getElementById('visor').innerHTML = '÷';
+                                                break;
+                                
+
+    }
+
+
+}
