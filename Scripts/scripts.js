54bfb54b6d3b88d370c282cbe1a9445d0be09877
di0nar4p <glaucoplay@live.com>
2023-04-19 12:53:52 -0300

    Função do keyboard parcialmente implementada

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -10,62 +10,70 @@ function keyboard(id){
             case 0:
                 document.getElementById('visor').innerHTML = 0;
                 break;
-
-                case 2:
-                    document.getElementById('visor').innerHTML = 2;
+             
+                case 1:
+                    document.getElementById('visor').innerHTML = 1;
                     break;
-                    
-                    case 3:
-                        document.getElementById('visor').innerHTML = 3;
+
+                    case 2:
+                        document.getElementById('visor').innerHTML = 2;
                         break;
                         
-                        case 4:
-                            document.getElementById('visor').innerHTML = 4;
+                        case 3:
+                            document.getElementById('visor').innerHTML = 3;
                             break;
-
-                            case 5:
-                                document.getElementById('visor').innerHTML = 5;
+                            
+                            case 4:
+                                document.getElementById('visor').innerHTML = 4;
                                 break;
 
-                                case 6:
-                                    document.getElementById('visor').innerHTML = 6;
+                                case 5:
+                                    document.getElementById('visor').innerHTML = 5;
                                     break;
 
-                                    case 7:
-                                        document.getElementById('visor').innerHTML = 7;
+                                    case 6:
+                                        document.getElementById('visor').innerHTML = 6;
                                         break;
 
-                                        case 8:
-                                            document.getElementById('visor').innerHTML = 8;
+                                        case 7:
+                                            document.getElementById('visor').innerHTML = 7;
                                             break;
-                                        
-                                            case 9:
-                                                document.getElementById('visor').innerHTML = 9;
+
+                                            case 8:
+                                                document.getElementById('visor').innerHTML = 8;
                                                 break;
-                                                
-                                                case '÷':
-                                                    document.getElementById('visor').innerHTML = '÷';
+                                            
+                                                case 9:
+                                                    document.getElementById('visor').innerHTML = 9;
                                                     break;
-                                                
-                                                    case 'x':
-                                                        document.getElementById('visor').innerHTML = 'x';
+                                                    
+                                                    case '÷':
+                                                        document.getElementById('visor').innerHTML = '÷';
                                                         break;
                                                     
-                                                        case '-':
-                                                            document.getElementById('visor').innerHTML = '-';
+                                                        case 'x':
+                                                            document.getElementById('visor').innerHTML = 'x';
                                                             break;
-                                                            
-                                                            case 'virg':
-                                                                document.getElementById('visor').innerHTML = ',';
-                                                                break;
-
-                                                                case 'C':
-                                                                    document.getElementById('visor').innerHTML = '';
-                                                                    break;
                                                         
-                                                            case '=':
+                                                            case '-':
+                                                                document.getElementById('visor').innerHTML = '-';
                                                                 break;
-                                        
+                                                                
+                                                                case 'virg':
+                                                                    document.getElementById('visor').innerHTML = ',';
+                                                                    break;
+
+                                                                    case 'C':
+                                                                        document.getElementById('visor').innerHTML = '';
+                                                                        break;
+
+                                                                        case '+':
+                                                                            document.getElementById('visor').innerHTML = '+';
+                                                                            break;
+                                                            
+                                                                            case '=':
+                                                                                break;
+                                            
 
     }
 
