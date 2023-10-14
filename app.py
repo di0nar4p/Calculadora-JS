48f007bb235867b1c0637e9075a25795fe8eb935
pedro Felipe <144541134+pedroFelipe11@users.noreply.github.com>
2023-10-14 12:57:06 -0300

    Atualização

--- app.py
+++ app.py
@@ -1,32 +1,43 @@
 
 from colorama import Fore
 
-def caucularadora ():
- return caucularadora
+def calcularadora ():
+ return calcularadora
 print(Fore.BLUE +'Olà seja bem vindo(a) a cauculadora')
-for numbers in range(2):
- numbers =    float( input(Fore.GREEN + f'Digite o { numbers+ 1}º numero:  '))
 
-print(Fore.BLUE + '''Digite o tipo de operaçäo que deseja fazer
+while True:
+ cauculos_feitos = 0 
+ for numbers in range(2):
+     numbers =    float( input(Fore.GREEN + f'Digite o { numbers+ 1}º numero:  '))
+
+ print(Fore.BLUE + '''Digite o tipo de operaçäo que deseja fazer
            [+] Adiçäo
            [-] Subtraçäo
            [/] Divisäo
            [x] Multiplicaçäo
            
            ''')
-cauculo = input('-> ').upper()
+ cauculo = input('-> ').upper()
  
 
-if cauculo == '+':
+ if cauculo == '+':
   print(Fore.GREEN +f'O resultado è {numbers + numbers:.2f}')
-elif cauculo == '-':
- print(Fore.GREEN +f'O resultado è {numbers - numbers:.2f}')
-elif cauculo == '/':
- print(Fore.GREEN +f'O resultado è {numbers / numbers:.2f}')
+ elif cauculo == '-':
+  print(Fore.GREEN +f'O resultado è {numbers - numbers:.2f}')
+ elif cauculo == '/':
+  print(Fore.GREEN +f'O resultado è {numbers / numbers:.2f}')
 
-elif cauculo == 'X':
- print(Fore.GREEN +F'O resultado è {numbers * numbers:.2f}')
-else:
- print( Fore.RED+'operaçäo INVALIDA ')
+ elif cauculo == 'X':
+  print(Fore.GREEN +F'O resultado è {numbers * numbers:.2f}')
+ else:
+  print('Operaçao invalida')
 
-print('Valeu por ter usado')
+ print('Deseja continuar os cauculos? S/N').upper()
+ opçao = input('-> ')
+
+ if opçao == 'S'and opçao =='sim':
+  print('Obrigado por ter usado')
+  break
+ else:
+  cauculos_feitos +=1
+
